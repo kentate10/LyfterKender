3c0597faaf28609485e45d4d96543e672f951ff4
kentate10 <kentate10@gmail.com>
2025-06-20 18:35:32 -0600

    ejercicion 1 semana 12

--- Semana11/#semana 12.py
+++ Semana11/#semana 12.py
@@ -12,18 +12,22 @@ class BankAccount:
         else:
             print("Deposit amount must be positive.")
     
-def withdraw(self, amount):
-    if amount > 0:
-        if self.balance - amount < self.minimum_balance():
-            print("Withdrawal denied. Balance would fall below minimum required.")
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.balance - amount < self.minimum_balance():
+                print("Withdrawal denied. Balance would fall below minimum required.")
+            else:
+                self.balance -= amount
+                print(f"Withdrew {amount}. New balance is {self.balance}.")
         else:
-            self.balance -= amount
-            print(f"Withdrew {amount}. New balance is {self.balance}.")
-    else:
-        print("Withdrawal amount must be positive.")
+            print("Withdrawal amount must be positive.")
  
 
 class SavingsAccount(BankAccount):
+    def __init__(self, account_number, balance=0, min_balance=1000):
+        super().__init__(account_number, balance) #Super para llamar al metodos de la clase padre y mandarle el account# y balance
+        self.min_balance = min_balance
     def minimum_balance(self):
-        return 1000
+        return self.min_balance
+
     
