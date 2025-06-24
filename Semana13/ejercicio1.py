c9aa568ea65cd6ecf63944de4c45b017553b5493
kentate10 <kentate10@gmail.com>
2025-06-24 12:40:08 -0600

    Se prueban los decoradores con los Args y Kwargs

---
+++ Semana13/ejercicio1.py
@@ -0,0 +1,29 @@
+
+def decorator_Return(func):
+    def wrapper(*args, **kwargs):
+        print("Aca entra el decorador")
+        result = func(*args, **kwargs) #se ejecuta la funcion original, la que vamos a decorar 
+        print(f"Los datos son: {result}")
+        print("Aca sale el decorador")
+        # se retorna el resultado de la funcion original
+        return result
+    return wrapper
+
+@decorator_Return # se decora la funcion test_decorator
+def test_decorator():
+    
+    personas = input("introduzca los nombres separados por comas: ")
+    personas = [persona.strip() for persona in personas.split(",")]
+
+    pelo= input("introduzca el color del pelo: ")
+    ojos = input("introduzca el color de los ojos: ")
+    altura = input("introduzca la altura: ")
+
+    return personas, {
+        "pelo": pelo,
+        "ojos": ojos,
+        "altura": altura
+    }
+
+test_decorator() # se ejecuta la funcion decorada
+~
